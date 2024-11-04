b3e64594810262c7ec4f60ec940fe3b78d91190c
tofu_wsl-20.04 <tamurabeta2020@gmail.com>
2024-11-04 15:09:35 +0900

    add: 山札を作成した

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+
+class PlayColor(Enum):
+    RED = 1
+    BLUE = 2
+    GREEN = 3
+
+class PlayCard:
+    def __init__(self, color: PlayColor, number: int):
+        self.color = color
+        self.number = number
+        self.validate()
+    def validate(self):
+        if isinstance(self.color, PlayColor) == False and isinstance(self.number, int) == False:
+            raise ValueError('Invalid type')
+        if self.number < 0 or self.number > 9:
+            raise ValueError('Number must be between 0 and 9')
+        if self.color == PlayColor.GREEN and self.number != 5:
+            raise ValueError('Green cards must have number 5')
+
+class PlayDeck:
+    def __init__(self):
+        self.cards = [
+            PlayCard(color, n) for n in range(10) for color in [PlayColor.RED, PlayColor.BLUE] if n != 5
+        ]
+        self.cards.append(PlayCard(PlayColor.GREEN, 5))
+        self.cards.append(PlayCard(PlayColor.GREEN, 5))
+
+if __name__ == "__main__":
+    deck = PlayDeck()
+    print(deck.cards)
